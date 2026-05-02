9622ca9e62023e7bf01be8a9b598086c1dbadeab
kaustubh739 <kaustubhwani95@gmail.com>
2026-05-02 22:24:00 +0530

    Add files via upload

---
+++ Program330.js
@@ -0,0 +1,7 @@
+let No1 = 10
+let No2 = 11;
+let ans = 0;
+
+ans = No1 + No2;
+
+console.log("Addition is : "+ans)
---
+++ Program337.js
@@ -0,0 +1,16 @@
+
+function main()
+{
+    let no1 = 10, no2 = 15;
+
+    if(no1 > no2)
+    {
+        console.log("no1 is Max");
+    }
+    else
+    {
+        console.log("no2 is Max");
+    }
+}
+
+main();
